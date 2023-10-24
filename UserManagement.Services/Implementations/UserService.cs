06110c8ce1c381a736d881f39c5ba3abc71a7497
Yan Fung <fungmingyan@hotmail.com>
2023-10-24 20:55:54 +0100

    Removed unnecessary using directives and implemented logic to filter users by their active state in the UserService class

--- UserManagement.Services/Implementations/UserService.cs
+++ UserManagement.Services/Implementations/UserService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UserManagement.Data;
 using UserManagement.Models;
 using UserManagement.Services.Domain.Interfaces;
@@ -18,7 +19,7 @@ public class UserService : IUserService
     /// <returns></returns>
     public IEnumerable<User> FilterByActive(bool isActive)
     {
-        throw new NotImplementedException();
+        return _dataAccess.GetAll<User>().Where(u => u.IsActive == isActive).ToList();
     }
 
     public IEnumerable<User> GetAll() => _dataAccess.GetAll<User>();
