23f8ced9ce230a4f11eb66a9d8bb7330052ab774
Yan Fung <fungmingyan@hotmail.com>
2023-10-24 22:14:56 +0100

    Implemented user filtering (active, inactive or all) in UsersController, along with improved button functionality for filtering in Users Views

--- UserManagement.Web/Controllers/UsersController.cs
+++ UserManagement.Web/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 using UserManagement.Services.Domain.Interfaces;
 using UserManagement.Web.Models.Users;
+using UserManagement.Models;
 
 namespace UserManagement.WebMS.Controllers;
 
@@ -11,9 +12,24 @@ public class UsersController : Controller
     public UsersController(IUserService userService) => _userService = userService;
 
     [HttpGet]
-    public ViewResult List()
+    public ViewResult List(bool? showActive = null)
     {
-        var items = _userService.GetAll().Select(p => new UserListItemViewModel
+        /// Get the list of users based on the showActive parameter
+        IEnumerable<User> users;
+
+        if (showActive.HasValue)
+        {
+            users = showActive.Value
+                ? _userService.FilterByActive(true)  /// Fetch active users
+                : _userService.FilterByActive(false); /// Fetch non-active users
+        }
+        else
+        {
+            /// If showActive is not specified, get all users
+            users = _userService.GetAll();
+        }
+
+        var items = users.Select(p => new UserListItemViewModel
         {
             Id = p.Id,
             Forename = p.Forename,
